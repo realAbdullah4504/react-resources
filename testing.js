adb5d92e5396ad9cb7fe66d12f74a803acdc69a2
Ubuntu <ubuntu@ip-172-31-22-247.ec2.internal>
2025-05-17 11:40:32 +0000

    environment setup

---
+++ testing.js
@@ -0,0 +1,95 @@
+function con(k)
+    {
+        let number=
+        {
+            num:2,
+            increment:function ()
+            {
+            return this.num+k;
+        },   
+    };
+        return number;
+    }
+let c=con(5);
+const increment=c.increment();
+
+
+//map filter reduce
+const products = [
+  { name: 'Laptop', price: 1200 },
+  { name: 'Phone', price: 800 },
+  { name: 'Tablet', price: 600 },
+  { name: 'Monitor', price: 300 },
+  { name: 'Keyboard', price: 100 }
+];
+
+
+const mapExample=products.map(product=>product.name)
+mapExample
+
+const filterExample=products.filter(product=>product.price>300 && product.price<800 )
+filterExample
+
+const reduceExample=products.reduce((acc,product)=>{
+    total=acc+product.price;
+    return total
+},0)
+reduceExample
+
+
+//destrucururing
+const product = { name: 'Mouse', price: 50 };
+
+const { name, price } = product;
+name
+price
+
+
+//spread and rest operators
+const users = [
+  { id: 1, name: "Alice", age: 25, email: "alice@example.com" },
+  { id: 2, name: "Bob", age: 30, email: "bob@example.com" },
+  { id: 3, name: "Charlie", age: 28, email: "charlie@example.com" },
+];
+const addUser={id:4,name:"John",email:"john@example.com"}
+
+const spreadUser={...users,addUser}
+spreadUser
+
+
+const {email,...rest}=users[0]
+rest
+
+const [firstUser, ...otherUsers] = users;
+otherUsers
+
+
+//deep copy shallow copy
+
+const people = [
+  { id: 1, name: "Alice", age: 25, email: "alice@example.com"},
+  { id: 2, name: "Bob", age: 30, email: "bob@example.com" },
+  { id: 3, name: "Charlie", age: 28, email: "charlie@example.com" },
+];
+
+const copyPeople={...people}
+// const copyPeople=people
+// const copyPeople = JSON.parse(JSON.stringify(people));
+
+copyPeople[0].name= "Abdullah"
+console.log(copyPeople[0].name)
+console.log(people[0].name)
+
+
+const user = {
+  name: "Alice",
+  age: 25,
+  email: "alice@example.com"
+};
+
+// Create a shallow copy
+const updatedUser = { ...user, name: "Alicia" };
+
+console.log(user.name);        
+console.log(updatedUser.name);
+
