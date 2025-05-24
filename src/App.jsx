6cfef6118e90534286127c774fe4bd003b239f7b
realAbdullah4504 <abdullahjaved4504@gmail.com>
2025-05-24 15:36:47 +0500

    assignment solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import React from 'react';
 import './App.css'
 import ContextComponent from './components/ContextComponent'
 import StateExample from './components/StateExample'
 import { NotesProvider } from './context/useNotes'
+import ReactElements from './components/reactElements';
 
 function App() {
 
+ 
   return (
     <div className='container'>
-      <StateExample />
+      <ReactElements/>
+      {/* <StateExample /> */}
       {/* <NotesProvider>
         <ContextComponent/>
       </NotesProvider> */}
