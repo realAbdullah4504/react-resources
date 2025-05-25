33b8610aa6505a2dbd46f2e34bd2c2d4fb3e3dfe
realAbdullah4504 <abdullahjaved4504@gmail.com>
2025-05-25 13:00:52 +0500

    fetching from api

--- src/pages/ProtectedPage.jsx
+++ src/pages/ProtectedPage.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const ProtectedPage = () => {
+  const [response, setResponse] = React.useState(null);
+  useEffect(() => {
+    // Simulate a side effect, like fetching data or logging
+    console.log('ProtectedPage component mounted');
+
+    (async () => {
+      try {
+        const response = await fetch('http://localhost:3001/users');
+        const data = await response.json();
+        setResponse(data);
+        console.log('Fetched data:', data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    })();
+
+    // Cleanup function if needed
+    return () => {
+      console.log('ProtectedPage component unmounted');
+    };
+  }, []);
   return (
     <div>
-      this is protected page
+      {JSON.stringify(response, null, 2) || 'Loading...'}
     </div>
   )
 }
