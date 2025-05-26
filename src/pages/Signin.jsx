dfbd26856136e0a75633f6c70e41aca34e192058
realAbdullah4504 <abdullahjaved4504@gmail.com>
2025-05-26 17:21:28 +0500

    signin form modified

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useAuth } from "../context/authContext";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Signin = () => {
   const navigate = useNavigate();
@@ -10,7 +10,7 @@ const Signin = () => {
   });
 
   const { login } = useAuth();
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -22,24 +22,20 @@ const Signin = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError('');
+    setError("");
     try {
       await login(formData.username, formData.password);
       console.log("Login successful");
       navigate("/protected-page");
     } catch (err) {
-      setError(err.message || 'Login failed. Please try again.');
+      setError(err.message || "Login failed. Please try again.");
     }
   };
 
   return (
     <div>
       <h2 className="text-2xl font-bold mb-6 text-center">Sign In</h2>
-      {error && (
-        <div className="text-red-500 text-center mb-4">
-          {error}
-        </div>
-      )}
+      {error && <div className="text-red-500 text-center mb-4">{error}</div>}
       <form
         className="max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-md"
         onSubmit={handleSubmit}
@@ -60,6 +56,9 @@ const Signin = () => {
           onChange={handleChange}
           value={formData.password}
         />
+        <div className="mb-4">
+          <Link to="/signup" className="text-blue-500 hover:underline">Don't have an account? Sign up</Link>
+        </div>
         <button
           type="submit"
           className="w-full bg-blue-500 text-white p-3 rounded hover:bg-blue-600 transition duration-200"
